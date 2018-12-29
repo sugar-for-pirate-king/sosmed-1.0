b5dc656761fdc59779a8775e4bdb9d23a9b21fad
FauzanIchwan <fauzanichwan99@gmail.com>
2018-12-29 09:35:08 +0700

    add profile page design

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -2,8 +2,29 @@
 
 @section('content')
 <div class="container">
-@foreach($user as $u)
-	{{ $u->name }}
-@endforeach
+	@foreach($user as $u)
+		<div class="jumbotron">
+			<img src="{{ asset('images/user.jpg') }}">
+			<h2>{{ $u->name }}</h2>
+			<p>{{ $u->username }}</p>
+		</div>
+	@endforeach
 </div>
 @endsection
+<style type="text/css">
+	.jumbotron{
+		background: url(../images/user.jpg);
+	}
+
+	.jumbotron img{
+		width: 200px;
+		height: 200px;
+		border-radius: 50%;
+		border: 7px solid white;
+	}
+
+	.jumbotron h2, p{
+		color: #fff;
+		margin-left: 5%;
+	}
+</style>
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/user/{user}', 'ProfileController@profile');
+Route::get('/user/{user}', 'ProfileController@profile')->name('profile');
